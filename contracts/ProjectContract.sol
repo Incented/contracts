25fe603cadbdd1fe36e491af3462a298b87a35b1
ujjwal <ujjwal@MacBook-Air-3.local>
2024-02-27 13:56:54 -0700

    Fixed the task mapping in the project contract

--- contracts/ProjectContract.sol
+++ contracts/ProjectContract.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./Taskcontract.sol";
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract EcosystemContract is Ownable, ReentrancyGuard, Initializable {
+contract ProjectContract is Ownable, ReentrancyGuard, Initializable {
+
+    mapping(string => address) tasks;
+
 
     bool public isInitialized;
     
@@ -43,68 +48,103 @@ contract EcosystemContract is Ownable, ReentrancyGuard, Initializable {
     }
 
      function addTask() external nonReentrant {
-        uint256 tokenId = totalSupply() + 1;
-        // console.logBytes(_proof[0]);
-
-        _safeMint(msg.sender, tokenId);
-
-        emit Mint(tokenId, msg.sender);
-
-        mintedTillNow++;
+       
     }
 
      function removeTask() external nonReentrant {
-        uint256 tokenId = totalSupply() + 1;
-        // console.logBytes(_proof[0]);
-
-        _safeMint(msg.sender, tokenId);
-
-        emit Mint(tokenId, msg.sender);
-
-        mintedTillNow++;
+       
     }
 
-    function mintNFT() external nonReentrant {
-        uint256 tokenId = totalSupply() + 1;
-        // console.logBytes(_proof[0]);
+  
+ function deployTaskClone(
+        address _implementationContract,
+        string memory _name,
+        string memory _taskSlug
+    ) external returns (address) {
+        require(
+            proxies[_taskSlug] == 0x0000000000000000000000000000000000000000,
+            "Task exists already with the same name, use different identifier"
+        );
 
-        _safeMint(msg.sender, tokenId);
+        // convert the address to 20 bytes
+        bytes20 implementationContractInBytes = bytes20(
+            _implementationContract
+        );
+        //address to assign a cloned proxy
+        address proxy;
 
-        emit Mint(tokenId, msg.sender);
+        // as stated earlier, the minimal proxy has this bytecode
+        // <3d602d80600a3d3981f3363d3d373d3d3d363d73><address of implementation contract><5af43d82803e903d91602b57fd5bf3>
 
-        mintedTillNow++;
+        // <3d602d80600a3d3981f3> == creation code which copies runtime code into memory and deploys it
+
+        // <363d3d373d3d3d363d73> <address of implementation contract> <5af43d82803e903d91602b57fd5bf3> == runtime code that makes a delegatecall to the implentation contract
+
+        assembly {
+            /*
+            reads the 32 bytes of memory starting at the pointer stored in 0x40
+            In solidity, the 0x40 slot in memory is special: it contains the "free memory pointer"
+            which points to the end of the currently allocated memory.
+            */
+            let clone := mload(0x40)
+            // store 32 bytes to memory starting at "clone"
+            mstore(
+                clone,
+                0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+            )
+
+            /*
+              |              20 bytes                |
+            0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+                                                      ^
+                                                      pointer
+            */
+            // store 32 bytes to memory starting at "clone" + 20 bytes
+            // 0x14 = 20
+            mstore(add(clone, 0x14), implementationContractInBytes)
+
+            /*
+              |               20 bytes               |                 20 bytes              |
+            0x3d602d80600a3d3981f3363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe
+                                                                                              ^
+                                                                                              pointer
+            */
+            // store 32 bytes to memory starting at "clone" + 40 bytes
+            // 0x28 = 40
+            mstore(
+                add(clone, 0x28),
+                0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
+            )
+
+            /*
+            |                 20 bytes                  |          20 bytes          |           15 bytes          |
+            0x3d602d80600a3d3981f3363d3d373d3d3d363d73b<implementationContractInBytes>5af43d82803e903d91602b57fd5bf3 == 45 bytes in total
+            */
+
+            // create a new contract
+            // send 0 Ether
+            // code starts at the pointer stored in "clone"
+            // code size == 0x37 (55 bytes)
+            proxy := create(0, clone, 0x37)
+        }
+
+        // Call initialization
+        TaskContract(proxy).initialize(_name, _taskSlug);
+
+        tasks[_taskSlug] = proxy;
+
+        emit AddTask(_name, proxy);
+
+        return proxy;
     }
 
+    
+    function getTasks(string memory _taskSlug) public view returns (address) {
+        require(
+            tasks[_taskSlug] != 0x0000000000000000000000000000000000000000,
+            "No Tasks exists with this name"
+        );
 
-
-    function tokenExists(uint256 tokenId) public view returns (bool) {
-        return _exists(tokenId);
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return mintedTillNow;
-    }
-
-    function tokenURI(uint256 _tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
-        _requireMinted(_tokenId);
-
-        //Gives the token metadata according to NFT campaigns
-        return
-            bytes(_baseImage).length > 0
-                ? string(abi.encodePacked(_baseImage))
-                : "";
-    }
-
-    function name() public view virtual override returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view virtual override returns (string memory) {
-        return _symbol;
+        return tasks[_taskSlug];
     }
 }
