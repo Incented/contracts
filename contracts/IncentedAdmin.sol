787da561f593d934321383f6d658814bc67ff092
7ayd <zalashini3@gatech.edu>
2024-02-27 00:01:46 -0700

    revert before creating new branch

--- contracts/IncentedAdmin.sol
+++ contracts/IncentedAdmin.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-contract IncentedAdmin {}
+//need to create a community
+contract IncentedAdmin {
+    mapping(string communityName => address communityAddress) public communityToAddress;
+}
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
